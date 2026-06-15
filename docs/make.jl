39a073002c93bbccf2c1705f53270aa086df2cd6
simonsteiger <simon.j.steiger@gmail.com>
2026-06-15 14:46:01 +0200

    build docs

--- docs/make.jl
+++ docs/make.jl
@@ -18,37 +18,35 @@ DocMeta.setdocmeta!(
             tsdmard::Bool
             cortisone::Bool
         end
-        DrugInterface.substance(d::ExampleDrug)      = d.name
+        DrugInterface.substance(d::ExampleDrug) = d.name
         DrugInterface.mode_of_action(d::ExampleDrug) = d.moa
-        DrugInterface.is_csdmard(d::ExampleDrug)     = d.csdmard
-        DrugInterface.is_bdmard(d::ExampleDrug)      = d.bdmard
-        DrugInterface.is_tsdmard(d::ExampleDrug)     = d.tsdmard
-        DrugInterface.is_cortisone(d::ExampleDrug)   = d.cortisone
-        MTX = ExampleDrug("Methotrexate", :none, true,  false, false, false)
-        ADA = ExampleDrug("Adalimumab",   :TNFi, false, true,  false, false)
-        TOF = ExampleDrug("Tofacitinib",  :JAKi, false, false, true,  false)
+        DrugInterface.is_csdmard(d::ExampleDrug) = d.csdmard
+        DrugInterface.is_bdmard(d::ExampleDrug) = d.bdmard
+        DrugInterface.is_tsdmard(d::ExampleDrug) = d.tsdmard
+        DrugInterface.is_cortisone(d::ExampleDrug) = d.cortisone
+        MTX = ExampleDrug("Methotrexate", :none, true, false, false, false)
+        ADA = ExampleDrug("Adalimumab", :TNFi, false, true, false, false)
+        TOF = ExampleDrug("Tofacitinib", :JAKi, false, false, true, false)
     end;
     recursive = true,
 )
 
 makedocs(;
-    modules  = [TreatmentTrajectories],
-    authors  = "Simon Steiger",
-    repo     = "https://github.com/simonsteiger/02_outcomes",
-    sitename = "TreatmentTrajectories.jl",
+    modules = [TreatmentTrajectories],
+    authors = "Simon Steiger",
+    repo = "https://github.com/simonsteiger/02_outcomes",
     format = DocumenterVitepress.MarkdownVitepress(
-        repo            = "https://github.com/simonsteiger/02_outcomes",
-        devbranch       = "main",
-        devurl          = "dev",
-        build_vitepress = false,
+        repo = "github.com/simonsteiger/TreatmentTrajectories.jl",
+        devbranch = "main",
+        devurl = "dev",
     ),
     pages = [
         "Home" => "index.md",
         "Guides" => [
-            "Intervals"           => "guides/intervals.md",
-            "Treatments"          => "guides/treatments.md",
-            "Windows & queries"   => "guides/windows.md",
-            "Episodes & lines"    => "guides/episodes-and-lines.md",
+            "Intervals" => "guides/intervals.md",
+            "Treatments" => "guides/treatments.md",
+            "Windows & queries" => "guides/windows.md",
+            "Episodes & lines" => "guides/episodes-and-lines.md",
         ],
         "API reference" => "api.md",
     ],
@@ -56,7 +54,8 @@ makedocs(;
 )
 
 DocumenterVitepress.deploydocs(;
-    repo         = "github.com/simonsteiger/TreatmentTrajectories.jl",
-    devbranch    = "main",
+    repo = "github.com/simonsteiger/TreatmentTrajectories.jl",
+    devbranch = "main",
+    target=joinpath(@__DIR__, "build"),
     push_preview = true,
 )
